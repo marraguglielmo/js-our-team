97e0f68cc933c30c4095ce2e5e08f937bcc03e06
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-03-05 15:27:32 +0100

    [add] readme, array, ciclo for in, prova console

---
+++ README.md
@@ -0,0 +1,18 @@
+# Our Team
+
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+#### **DATI**
+
+Wayne Barnett\_\_\_\_ Founder & CEO\_\_\_\_ wayne-barnett-founder-ceo.jpg <br>
+Angela Caroll\_\_\_\_ Chief Editor\_\_\_\_ angela-caroll-chief-editor.jpg <br>
+Walter Gordon\_\_\_\_ Office Manager\_\_\_\_ walter-gordon-office-manager.jpg <br>
+Angela Lopez\_\_\_\_ Social Media Manager\_\_\_\_ angela-lopez-social-media-manager.jpg <br>
+Scott Estrada\_\_\_\_ Developer\_\_\_\_ scott-estrada-developer.jpg <br>
+Barbara Ramos\_\_\_\_ Graphic Designer\_\_\_\_ barbara-ramos-graphic-designer.jpg <br>
+
+## Svolgimento
+
+1. creo un array di oggetti con i dati per ogni membro
+2. con un ciclo for in stampo i dati
---
+++ js/script.js
@@ -0,0 +1,45 @@
+const members =[
+
+    {
+        nome : 'Wayne Bernett',
+        ruolo : 'Founder & CEO',
+        foto : 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        nome : 'Angela caroll',
+        ruolo : 'Chief Editor',
+        foto : 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        nome : 'Walter Gordon',
+        ruolo : 'Office Manager',
+        foto : 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        nome : 'Angela Lopez',
+        ruolo : 'Social Media Manager',
+        foto : 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        nome : 'Scott Estrada',
+        ruolo : 'Developer',
+        foto : 'scott-estrada-developer.jpg'
+    },
+
+    {
+        nome : 'Barbara Ramos',
+        ruolo : 'Graphic Designer',
+        foto : 'barbara-ramos-graphic-designer.jpg'
+    },
+];
+
+for(let member in members){
+    console.log(members[member].nome + '--> ' + members[member].ruolo);
+}
+
+
+
